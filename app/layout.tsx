fa55469c2d3b67a7fd080d186d920106547ba891
benwxng <benxwang7@gmail.com>
2025-09-27 14:02:19 -0400

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Origins - Remember your roots",
+  description:
+    "Origins is a platform for families to share their stories and memories.",
 };
 
 const geistSans = Geist({
